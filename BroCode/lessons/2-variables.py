82f46ea38f5cf85f615f36b39e81740a36ffd7aa
Sofia KN <skhlebn@gmail.com>
2021-07-23 01:34:25 -0300

    (BroCode): using variables

---
+++ BroCode/lessons/2-variables.py
@@ -0,0 +1,26 @@
+# variable = a container for a value.
+# Behaves as the value that it contains
+
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name +" "+ last_name
+#name='Bro'
+
+print("Hello "+full_name)
+#print(type(name))
+
+age = 21
+age += 1
+print("Your age is: "+str(age))
+#print(age)
+#print(type(age))
+
+height = 250.5
+print("Your height is: "+str(height)+"cm")
+#print(height)
+#print(type(height))
+
+human = True
+print("Are you a human: "+str(human))
+#print(human)
+#print(type(human))
