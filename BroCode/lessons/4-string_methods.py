441cf70f84a5daf5c755ff617fab5428d5804155
Sofia KN <skhlebn@gmail.com>
2021-07-23 01:46:48 -0300

    (BroCode): string methods

---
+++ BroCode/lessons/4-string_methods.py
@@ -0,0 +1,13 @@
+#name = "Bro Code"
+name = "Bro"
+
+#print(len(name))          # 8
+#print(name.find("o"))     # 2
+#print(name.capitalize())  # "bro code" -> "Bro code"
+#print(name.upper())       # "bro code" -> "BRO CODE"
+#print(name.lower())       # "Bro Code" -> "bro code"
+#print(name.isdigit())     # False
+#print(name.isalpha())     # False, because it has space between ..o and C...
+#print(name.count("o"))    # 2
+#print(name.replace("o","a"))  # "Bro Code" -> "Bra cade"
+print(name*3)              # "Bro" -> "BroBroBro"
