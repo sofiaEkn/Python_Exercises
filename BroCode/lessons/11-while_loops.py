74bd3cf004c2082da2fd7cf4adc6f1f217f55e53
Sofia KN <skhlebn@gmail.com>
2021-08-02 00:01:42 -0300

    (BroCode): while loops

---
+++ BroCode/lessons/11-while_loops.py
@@ -0,0 +1,19 @@
+# while loop = a statement that will execute it's of code,
+#              as long as it's condition remains true
+
+#while 1==1:
+  #name = print("Help! I'm stuck in a loop")
+
+#name = ""
+
+#while len(name) == 0:
+  #name = input("Enter your name: ")
+
+#print("Hello "+name)
+
+name = None
+
+while not name:
+  name = input("Enter your name: ")
+
+print("Hello "+name)
