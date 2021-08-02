daa07ae6a9bd60cc535b9809817b227557b4065d
Sofia KN <skhlebn@gmail.com>
2021-08-01 23:59:05 -0300

    (BroCode): user input

---
+++ BroCode/lessons/6-user_input.py
@@ -0,0 +1,9 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))  # cast type to must sum
+height = float(input("How tall are you?: "))
+
+age = age + 1
+
+print("Hello "+name)
+print("You are "+str(age)+" years old")
+print("You are "+str(height)+"cm tall")
