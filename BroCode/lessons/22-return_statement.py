5749363c63c3a76cca8305de84861d238de5cea9
Sofia KN <skhlebn@gmail.com>
2022-02-09 23:35:58 -0300

    (BroCode): return value

---
+++ BroCode/lessons/22-return_statement.py
@@ -0,0 +1,12 @@
+# return statement = Functions send Python values/objects back to the caller.
+#                    These values/objects are known as the function's return value
+
+def multiply(number1,number2):
+  #result = number1 * number2
+  #return result
+  return number1 * number2
+
+#print(multiply(6,8))
+x = multiply(6,8)
+
+print(x)
