5263050b4b9fa87093f1fa7257568027f94d438a
Sofia KN <skhlebn@gmail.com>
2022-02-09 23:30:18 -0300

    (BroCode): using sets

---
+++ BroCode/lessons/18-sets.py
@@ -0,0 +1,28 @@
+# set = collection which is unordered, unindexed. No duplicate values
+
+utensils = {"fork","spoon","knife"}
+
+#for x in utensils:
+#  print(x)
+
+#utensils.add("napkin")
+#utensils.remove("fork")
+#utensils.clear()
+
+dishes = {"bowl","plate","cup","knife"}
+
+#dishes.update(utensils)  #print: fork,spoon,knife,bowl,plate,cup
+
+#for x in dishes:
+#  print(x)
+
+#dinner_table = utensils.union(dishes)
+
+#for x in dinner_table:
+#  print(x)
+
+#print(utensils.difference(dishes))   # {fork, spoon}
+#print(dishes.difference(utensils))   # {bowl,plate,cup}
+#print(utensils.intersection(dishes)) # {knife}
+
+
