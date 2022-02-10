5a162c8687296ae56ba7a7b0a911ba5139bf77fe
Sofia KN <skhlebn@gmail.com>
2022-02-09 23:28:40 -0300

    (BroCode): 2D lists

---
+++ BroCode/lessons/16-2D_lists.py
@@ -0,0 +1,12 @@
+# 2D lists = a list of lists
+
+drinks = ["coffee","soda","tea"]
+dinner = ["pizza","hamburger","hotdog"]
+dessert = ["cake","ice cream"]
+
+food = [drinks,dinner,dessert]
+
+print(food)
+#print(food[0])  ["coffee","soda","tea"]
+#print(food[0][1]) "soda"
+#print(food[1][2]) "hotdog"
