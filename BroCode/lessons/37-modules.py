f689f7a3b63284582853847178077e2fe2f26173
Sofia KN <skhlebn@gmail.com>
2022-02-09 23:44:47 -0300

    (BroCode): program with modules

---
+++ BroCode/lessons/37-modules.py
@@ -0,0 +1,22 @@
+
+# module = a file containing python code. May contain functions, clases, etc.
+# used with modular programming, which is to separate a program into parts
+
+# import messages37
+# import messages37 as msg
+# from messages37 import hello,bye
+# from messages37 import * (this one is dangerous)
+
+#import messages37
+#messages37.hello()
+#messages37.bye()
+
+#import messages37 as msg
+#msg.hello()
+#msg.bye()
+
+# from messages37 import hello,bye
+#hello()
+#bye()
+
+#help ("modules")  #show all the modules
