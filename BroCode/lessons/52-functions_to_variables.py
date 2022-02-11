ef2db302cb90f6b40fe4e31d262f8e1f1bae8889
Sofia KN <skhlebn@gmail.com>
2022-02-11 02:39:03 -0300

    (BroCode): using functions as variables

---
+++ BroCode/lessons/52-functions_to_variables.py
@@ -0,0 +1,12 @@
+def hello():
+  print("Hello!")
+
+#hello()
+#print(hello)  #print memory address of function <function hello at 0x0225D850>
+
+#hi = hello  #assign memory address
+#hi()  #imprime Hello!
+#hello()  #imprime Hello!
+
+say = print
+say("Whoa! I can't believe this works! :0 ")
